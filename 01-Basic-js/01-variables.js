3e4e4f7c1434f5ff83895cb475f0b5c486ae1f4a
divyathakur134 <61382571+divyathakur134@users.noreply.github.com>
2025-06-27 10:43:32 +0000

    variables in javascript

---
+++ 01-Basic-js/01-variables.js
@@ -0,0 +1,11 @@
+const custId = 12;
+let custname = 'Divya';
+var balance = 1200;
+let account;  //undefined
+
+//custId = 11; // valur can't change for const
+custname = 'Anuj';
+//let balance = 100; //  balance can't be declare again
+
+console.log(custId,custname,balance,account);
+
