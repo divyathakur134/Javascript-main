75cd83883bae534ed00cc22f4c549a921e3ad6da
divyathakur134 <61382571+divyathakur134@users.noreply.github.com>
2025-07-17 09:24:57 +0000

    Dattypes

---
+++ 01-Basic-js/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Divya"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 9
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
