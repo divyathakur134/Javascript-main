8551a6fe985740eefaf8da9814348ca5ab217f94
divyathakur134 <61382571+divyathakur134@users.noreply.github.com>
2025-08-06 07:43:14 +0000

    Prototypes and adding new methods to the prototyoe

---
+++ 07-Javascript-OOPs/3-Prototype.js
@@ -0,0 +1,67 @@
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.Divya = function(){
+    console.log(`Divya is present in all objects`);
+}
+
+Array.prototype.heyDivya = function(){
+    console.log(`Divya says hello`);
+}
+
+// heroPower.Divya()
+// myHeros.Divya()
+// myHeros.heyDivya()
+// heroPower.heyDivya()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "Javascript     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
